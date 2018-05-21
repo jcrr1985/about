bb79ccf8c27f9dd9439a71e76bf9c0d28ca91b4a
Julio Rodriguez <julio.rodriguezr1@gmail.com>
2018-05-21 17:53:48 -0400

    4th commit

--- init.js
+++ init.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
 	$("div:last").css("display", "none");
 		$('.tlt').textillate({
 		in: { effect: 'rollIn', delay:20},
+		out: { effect:'rollIn'},
+		loop: true
 	});	
-}) 
+});
+
+$('.hexagono1').click(function(){
+  $(this).addClass('HexBig')
+});	
+
