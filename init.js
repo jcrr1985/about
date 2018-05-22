e3b91ab09aca569f399ce3b33942e2f058b578e4
Julio Rodriguez <julio.rodriguezr1@gmail.com>
2018-05-22 00:11:38 -0400

    next step: speed up the fadeIn animations and left the  triangle span text

--- init.js
+++ init.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	$("div:last").css("display", "none");
 		$('.tlt').textillate({
-		in: { effect: 'rollIn', delay:20},
-		out: { effect:'rollOut'},
-		loop: true
-	});	
+		in: { effect: 'rollIn', delay:20}
+	});
+
+	$(".modais").iziModal({ history: false, iframe : true, fullscreen: true, loop: true, zindex: 99999, focusInput: true });	
+
 });
 
 /*$('.triCon').hover(function(){
